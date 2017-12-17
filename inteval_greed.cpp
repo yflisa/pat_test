bda22443ca2b131fad1029c1edceaefa698419a2
fanyun <fanyun@kedacom.com>
2017-12-17 16:08:58 +0800

    贪心算法（一）
    区间贪心

---
+++ inteval_greed.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+typedef struct tagSegment
+{
+	int x;
+	int y;
+}Segment;
+
+bool cmp(Segment a, Segment b)
+{
+	if(a.x != b.x)
+		return a.x > b.x;
+	else
+		return a.y < b.y;
+}
+
+int main()
+{
+	Segment tSegment[10] = {0};
+	int nNum = 0;
+
+	while(scanf("%d", &nNum), nNum != 0)
+	{
+		for(int i = 0; i < nNum; i++)
+		{
+			scanf("%d %d",&tSegment[i].x, &tSegment[i].y);
+		}
+
+		sort(tSegment, tSegment + nNum , cmp);
+		int nAns = 1;
+		int nLastX = tSegment[0].x;
+		for(int i = 1; i < nNum; i++)
+		{
+			if(tSegment[i].y < nLastX)
+			{
+				nLastX = tSegment[i].x;
+				nAns++;
+			}
+		}
+		printf("%d\n", nAns);
+	}
+	return 0;
+}
