668402a7146220df1cadfc7b86e9ff9f74a9be2a
fanyun <fanyun@kedacom.com>
2017-12-20 14:33:22 +0800

    find prime within 100 (method 2)

---
+++ find_prime.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+const int maxn = 101;
+int prime[maxn], pNum = 0;
+bool p[maxn] = {0};
+
+void Find_Prime()
+{
+	for(int i = 2; i < maxn; i++)
+	{
+		if(p[i] == false)
+		{
+			prime[pNum++] = i;
+			for(int j = i; j < maxn; j += i)
+				p[j] = true;
+		}
+	}
+}
+
+int main()
+{
+	Find_Prime();
+	for(int i = 0; i < pNum; i++)
+		printf("%d ", prime[i]);
+	return 0;
+}
