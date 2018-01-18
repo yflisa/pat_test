9eb7e0f139576cb3c6a76fcf97d9f844e04d3fd7
fanyun <fanyun@kedacom.com>
2018-01-18 09:51:24 +0800

    pat test

--- B_1024.cpp
+++ B_1024.cpp
@@ -1,21 +1,26 @@
+/*
+	注意：
+	这里注意当次数为负的时候，下标从1开始，原因，在输出0之前已经输出了0.，如果从0开始，就多输了
+*/
+
 #include <cstdio>
 
 int main()
 {
 	char head, a[10001] = {0};
 	int ex;
+
 	scanf("%c%c.%[0-9]E%d", &head, &a[0], a+1, &ex);
 
 	if(head == '-')
 		printf("-");
-
 	if(ex >= 0)
 	{
-		for(int i = 0; i < ex + 1 || a[i] != 0; i++)
+		for(int i = 0; i < (ex + 1) || a[i] != 0; i++)
 		{
 			if(i == ex + 1)
 				printf(".");
-			printf("%c", a[i] == 0?'0':a[i]);
+			printf("%c", a[i] == 0 ? '0':a[i]);
 		}
 	}
 	else
@@ -25,6 +30,6 @@ int main()
 			printf("0");
 		printf("%s", a);
 	}
-	printf("\n");
+
 	return 0;
 }
