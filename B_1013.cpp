4b4c6e49745028beb99ea5466a17fa0d45ba7c5a
fanyun <fanyun@kedacom.com>
2018-02-07 09:48:43 +0800

    pat test
    note:
    筛法计算

--- B_1013.cpp
+++ B_1013.cpp
@@ -1,43 +1,39 @@
 #include <cstdio>
-using namespace std;
-#define MAX (100010)
+#include <cmath>
+int n, m, num = 0;
+const int maxn = 10010;
+bool prime[maxn] = {false};
+int is_prime[maxn] = {0};
 
-int nPrime[MAX] = {0};
-int nNum = 0;
-bool bIsPrime[MAX] = {false};
-
-void FindPrime(int nMax)
+void find_prime(int n)
 {
-	for(int i = 2; i < MAX; i++)
+	for(int i = 2; i < 1010; i++)
 	{
-		if(bIsPrime[i] == false)
+		if(prime[i] == false)
 		{
-			nPrime[nNum++] = i;
-			if(nNum >= nMax)
-				break;
-			for(int j = i + i; j < MAX; j += i)
+			is_prime[num++] = i;
+			for(int j = i + i; j < 1010; j += i)
 			{
-				bIsPrime[j] = true;
+				prime[j] = true;
 			}
+			if(num > n)
+				break;
 		}
 	}
 }
 int main()
 {
-	int nMin = 0, nMax = 0, nCount = 0;
-	scanf("%d %d", &nMin, &nMax);
-	FindPrime(nMax);
-	for(int i = nMin; i <= nMax; i++)
+	int count = 0;
+	scanf("%d%d", &m, &n);
+	find_prime(n);
+	for(int i = m; i <= n; i++)
 	{
-		printf("%d",nPrime[i - 1]);
-		nCount++;
-		if((nCount % 10 == 0) && (i < nMax))
+		printf("%d", is_prime[i - 1]);
+		count++;
+		if(count % 10 == 0)
 			printf("\n");
-		else if((nCount % 10 != 0) && (i < nMax))
+		else if(count % 10 != 0 && i < n)
 			printf(" ");
-		else if(i == nMax);
-			
 	}
-	
 	return 0;
 }
