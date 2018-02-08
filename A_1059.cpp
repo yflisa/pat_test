a87bc171652acbe2d990fa59aa48fc9c2162ed6b
fanyun <fanyun@kedacom.com>
2018-02-08 10:49:15 +0800

    pat test
    note：
    1.这里有1=1的一种特殊情况特判；
    2.当无法被根号n除尽的时候，有一个大于根号n的质因子，就是n本身（最后一个测试点）

--- A_1059.cpp
+++ A_1059.cpp
@@ -1,78 +1,62 @@
 #include <cstdio>
 #include <cmath>
 typedef long long LL;
-#define MAX 10010
-typedef struct tagFactor
+struct Factor
 {
-	int x;
-	int num;
-}Factor;
-
-int nNum = 0, nPrime[10010] = {0};
-bool bIsPrime[10010] = {false};
-//找出前十个质因子
-void Find_Prime()
-{
-	for(int i = 2; i < 10010; i++)
-	{
-		if(bIsPrime[i] == false)
-		{
-			nPrime[nNum++] = i;
-			for(int j = i + i; j < 10010; j += i)
-			{
-				bIsPrime[j] = true;
-			}
-		}
-	}
-}
-
-//循环查找相应的质因子
-void Find_Prime_Factor(LL n, Factor tFactor[])
-{
-	Find_Prime();
-	int nCount = 0;
-
-	for(int i = 0; i < nNum; i++)
-	{
-		if(n % nPrime[i] == 0)
-		{
-			 tFactor[nCount].num++;
-			 n /= nPrime[i];
-			 while(n % nPrime[i] == 0)
-			 {
-				 tFactor[nCount].num++;
-				 n /= nPrime[i];
-			 }
-			 tFactor[nCount++].x = nPrime[i];
-		}
-	}
-}
-
+	int p;
+	int k;
+}F[1100];
+int num = 0;
 int main()
 {
-	Factor tFactor[10] = {0};
 	LL n;
 	scanf("%lld", &n);
-	if(n == 1)
+	if(n == 0)
 	{
-		printf("1=1\n");
+		printf("0=0");
+		return 0;
+	}
+	else if(n == 1)
+	{
+		printf("1=1");
 		return 0;
 	}
 
-	Find_Prime_Factor(n ,tFactor);
-	printf("%d=", n);
-	for(int i = 0; i < 10; i++)
+	LL tmp = n;
+	LL sqr = (LL)sqrt(1.0 * n);
+	for(int i = 2; i <= sqr; i++)
 	{
-		if(tFactor[i].x == 0)
-			break;
-		if(i > 0)
-			printf("*");
-		if(tFactor[i].x != 0)
+		if(tmp % i == 0)
 		{
-			printf("%d", tFactor[i].x);
-			if(tFactor[i].num > 1)
-				printf("^%d", tFactor[i].num);
+			F[num].p = i;
+			F[num].k = 1;
+			tmp = tmp / i;
+			while(1)
+			{
+				if(tmp % i == 0)
+					F[num].k++;
+				else
+					break;
+				tmp /= i;
+			}
+			num++;
 		}
+	}
+	if(tmp != 1)
+	{
+		F[num].p = tmp;
+		F[num].k = 1;
+		num++;
+	}
+	printf("%lld=", n);
+	for(int i = 0; i < num; i++)
+	{
+		if(i == 0)
+			printf("%d", F[i].p);
+		else
+			printf("*%d", F[i].p);
+		if(F[i].k > 1)
+			printf("^%d", F[i].k);
 	}
 	return 0;
 }
