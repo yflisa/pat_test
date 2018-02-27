cab933049c716ef90526e5d393d4ea33effb8d11
fanyun <fanyun@kedacom.com>
2018-02-27 16:47:48 +0800

    pat test
    note：
    第二个测试点已经通过

--- A_1097.cpp
+++ A_1097.cpp
@@ -1,76 +1,62 @@
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
 using namespace std;
-const int maxn = 100010;
+const int maxn = 100005;
+const int TABLE = 100010;
 struct Node
 {
-	int addr, data, next, count;
-	bool flag;
+	int address, data, next;
+	int order;
 }node[maxn];
-bool hashtable[maxn] = {false};
+
+bool isExist[TABLE] = {false};
+bool cmp(Node a, Node b)
+{
+	return a.order < b.order;
+}
 
 int main()
 {
-	int s, n, addr;
-	scanf("%d %d", &s, &n);
+	memset(isExist, false, sizeof(isExist));
+	for(int i = 0; i < maxn; i++)
+		node[i].order = 2 * maxn;
+
+	int n, begin, address;
+	scanf("%d%d", &begin, &n);
 	for(int i = 0; i < n; i++)
 	{
-		scanf("%d", &addr);
-		scanf("%d %d", &node[addr].data, &node[addr].next);
-		node[addr].addr = addr;
+		scanf("%d", &address);
+		scanf("%d%d", &node[address].data, &node[address].next);
+		node[address].address = address;
 	}
-	int count = 0;
-	for(int i = s; i != -1; i = node[i].next)
+
+	int countValid = 0, countRemoved = 0, p = begin;
+	while(p != -1)
 	{
-		if(hashtable[abs(node[i].data)] == false)
+		if(!isExist[abs(node[p].data)])
 		{
-			hashtable[abs(node[i].data)] = true;
-			node[i].flag = true;
-			count++;
+			isExist[abs(node[p].data)] = true;
+			node[p].order = countValid++;
 		}
-	}
-	
-	bool flag = false;
-	for(int i = s, num = 0; i != -1; i = node[i].next)
-	{
-		if(node[i].flag == true)
+		else
 		{
-			if(flag == true)
-			{
-				flag = false;
-				printf("%05d\n", node[i].addr);
-			}
-			if(num + 1 != count)
-			{
-				printf("%05d %d ", node[i].addr, node[i].data);
-				flag = true;
-			}
-			else
-			{
-				printf("%05d %d -1\n", node[i].addr, node[i].data);
-				break;
-			}
-			num++;
+			node[p].order = maxn + countRemoved++;
 		}
+		p = node[p].next;
 	}
-	flag = false;
-	for(int i = s, num = count; i != -1; i = node[i].next)
+
+	sort(node, node + maxn, cmp);
+	int count = countValid + countRemoved;
+	for(int i = 0; i < count; i++)
 	{
-		if(node[i].flag == false)
+		if(i != countValid - 1 && i != count - 1)
 		{
-			if(flag == true)
-			{
-				flag = false;
-				printf("%05d\n", node[i].addr);
-			}
-			if(num + 1 != n)
-			{
-				printf("%05d %d ", node[i].addr, node[i].data, node[node[i].next].addr);
-				flag = true;
-			}
-			else
-				printf("%05d %d -1\n", node[i].addr, node[i].data);
-			num++;
+			printf("%05d %d %05d\n", node[i].address, node[i].data, node[i+1].address);
+		}
+		else
+		{
+			printf("%05d %d -1\n", node[i].address, node[i].data);
 		}
 	}
 	return 0;
